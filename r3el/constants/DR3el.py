09dc935dc6d6e8d42b757102978a52b5648b58e0
Nadim Ghaznavi <nghaznavi@gmail.com>
2026-09-26 05:41:23 -0400

    Release 4.6.0: TV Show Search

--- r3el/constants/DR3el.py
+++ r3el/constants/DR3el.py
@@ -5,7 +5,7 @@ class DR3el:
     RAW_LOGS_ENABLED: Final[bool] = False
     HTTP_TIMEOUT_SECONDS: Final[int] = 300
 
-    VERSION: Final[str] = "4.5.3"
+    VERSION: Final[str] = "4.6.0"
 
     BASE_DIR: Final[str] = "/opt/prod/r3el"
     FILM_DIR: Final[str] = "/exports/disk1/archive/film"
